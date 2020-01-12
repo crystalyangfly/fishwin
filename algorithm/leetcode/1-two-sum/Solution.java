6a36553e9109f3c65c4323bca65ba37a1e0a196c
crystalyang <crystalyangwj@163.com>
2020-01-12 22:03:53 +0800

    leetcode: 1 two sum

---
+++ algorithm/leetcode/1-two-sum/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        if (nums == null || nums.length == 0)
+            return res;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hm.containsKey(nums[i])) {
+                res[0] = hm.get(nums[i]);
+                res[1] = i;
+                return res;
+            } else {
+                hm.put(target - nums[i], i);
+            }
+        }
+        return res;
+    }
+}
